d507a50ac54bd1d6c7e4ff61e8cc878b357f680b
Cari Roberts <cari.roberts@ou.edu>
2017-04-12 15:43:40 -0500

    huge changes to tasks.pl

--- tasks.pl
+++ tasks.pl
@@ -1,21 +1,11 @@
 #!/usr/bin/perl 
 
-#$fileinput = shift(@ARGV);
-
-#no argument defined, no run script
-#if (length($fileinput) == 0) {
-        die "** Usage: scriptname.pl filename\n";
-   }
-
 #for Perl purposes, we need to call a XML parsing package, so...
 use XML::LibXML;
 
 #now for some file operations
-#specify the file to open, that value coming from the script runner, as well as an output filename. Also, let's kill the script gracefully if it can't create a file
-open(INFILE,$fileinput);
-open(OUTFILE,">modified_$fileinput") || die "cannot create output file\n";
-#assigning reject filename to variable for future function
-$rejectFileName = "./rejects_$fileinput";
+#specify the output filename. Also, let's kill the script gracefully if it can't create a file
+open(OUTFILE,">outfile") || die "cannot create output file\n";
 open(OUTFILE2,">$rejectFileName") || die "cannot create output file\n";
 binmode OUTFILE, ":utf8";
 
@@ -35,73 +25,53 @@ while(<INFILE>) {
     chomp;
 
     #we will be running an API call to retrieve Patron Physical Item requests for Storage
-    #our current incoming lines look like this, separated by tabs:  Library  Circ Desk
-    #so, the following command uses split to assign these values to the specified variable names
-    #($library,$circ_desk) = split /\t/;
-    $taskListLoc = "./itemXML/tasklist_$mmsID.xml";
-    if (-e $taskListLoc) {
-       #something
-    } else {
-      system("curl -L --request GET 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/task-lists/requested-resources?library=STORAGE&circ_desk=DEFAULT_CIRC_DESK&limit=10&offset=0&apikey=l7xx61b940d9c211450e99c500b5bfbbaf92' > $taskListLoc");
-    }
+
+      system("curl -L --request GET 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/task-lists/requested-resources?library=STORAGE&circ_desk=DEFAULT_CIRC_DESK&limit=10&offset=0&apikey=l7xx61b940d9c211450e99c500b5bfbbaf92' > requeststasklist");
     
     #we’re going to grab each url for a specific request and plug it into the next line
      my $parser = XML::LibXML->new();
      
-     my $holdinglistdoc = $parser->parse_file($holdingsListLoc);
-     foreach my $holdingLab ($holdinglistdoc->findnodes('/holdings/holding')) {
-       $holdingID = $holdingLab->findnodes('./holding_id')->to_literal();
-       $itemListLoc = "./itemXML/iteminfo_$mmsID\_$holdingID.xml";
-     if (-e $itemListLoc) {
-       #something
-     } else {
-       system("curl -L --request GET 'https://api-eu.hosted.exlibrisgroup.com/almaws/v1/bibs/$mmsID/requests?request_type=HOLD&status=active&apikey=l7xxa730c5d8d8844676ab5e1607d1161322' > $itemListLoc");  
+     my $holdinglistdoc = $parser->parse_file("requesttasklist");
+     foreach my $holdingLib ($holdinglistdoc->findnodes('/requested_resource/resource_metadata')) {
+       $mmsID = $holdingLib->findnodes('./mms_id')->to_literal();
+       $titlerequestinfo = "./itemXML/titlerequestinfo_$mmsID.xml";
+
+#second API call to get the actual items that have been requested
+
+       system("curl -L --request GET 'https://api-eu.hosted.exlibrisgroup.com/almaws/v1/bibs/$mmsID/requests?request_type=HOLD&status=active&apikey=l7xxa730c5d8d8844676ab5e1607d1161322' > $titlerequestinfo");  
      } 
 
       #assign the incoming file to a parser-related variable
-      my $itemdoc = $parser->parse_file($itemListLoc);
+      my $itemdoc = $parser->parse_file($titlerequestinfo);
       #probably important to note at this point you need to know the general XML structure so you know what/where to get desired data in the next few commands
       #let us create a for/each loop for item sections in the XML, and then a sub loop for each interesting section
       #in the sub-loops, we'll fetch values in which we are interested
       #should only be one of each item, holdingdata, and itemdata section, but this is useful logic when you have a doc with many items and repeating subsections
-      foreach my $itemrecord ($itemdoc->findnodes('/items/item')) {
-
-        #we need a block for the bibdata
-        foreach my $bibDataSec ($itemrecord->findnodes('./bib_data')) {
-          $title = $bibDataSec->findnodes('./title')->to_literal();
-	  $author = $bibDataSec->findnodes('./author')->to_literal();
-        }
-
-        #we need copy_ID, holding_ID from holding_data section
-        foreach my $holdingDataSec ($itemrecord->findnodes('./holding_data')) {
-          $copyID = $holdingDataSec->findnodes('./copy_id')->to_literal();
-          $holdingID = $holdingDataSec->findnodes('./holding_id')->to_literal();
-	  $callnum = $holdingDataSec->findnodes('./call_number')->to_literal();
-        }
-        #for copyID, we really only care if it's greater than one, so we're going to add it to the callnum variable if greater than one
-        if ($copyID ne "1") {
-          $callnum = $callnum . " copy $copyID";
-        }
-        #we need pid, description, physical_material_type from item_data section
-        foreach my $itemDataSec ($itemrecord->findnodes('./item_data')) {
-          $pid = $itemDataSec->findnodes('./pid')->to_literal();
-	  $barcode = $itemDataSec->findnodes('./barcode')->to_literal();
-          $description = $itemDataSec->findnodes('./description')->to_literal();
-          $physMatType = $itemDataSec->findnodes('./physical_material_type')->to_literal();
-	  $comment = $itemDataSec ->findnodes('./comment') ->to_literal();
-        }
+      #we are looking to grab each user request that currently exists relating to this title
+      foreach my $itemrecord ($itemdoc->findnodes('/user_requests/user_request')) {
+    
+          $title = $itemrecord->findnodes('./title')->to_literal();
+ 	  $userid = $itemrecord->findnodes('./user_primary_id')->to_literal();
+	  $requestid = $itemrecord->findnodes('./request_id')->to_literal();
+	  $description = $itemrecord->findnodes('./description')->to_literal();
+	  $barcode = $itemrecord->findnodes('./barcode')->to_literal();
+	  $physMatType = $itemrecord->findnodes('./material_type')->to_literal();
+	  $comment = $itemrecord->findnodes('./comment')->to_literal();
+	
 	  if (length($barcode)>0) {
+	  
+	  
+	  
+	  }
             print OUTFILE "$mmsID\t$holdingID\t$pid\t$title\t$author\t$callnum\t$description\t$barcode\t$physMatType\n";
-          } else {
-            print OUTFILE2 "$mmsID\t$holdingID\t$pid\t$title\t$author\t$callnum\t$description\t$barcode\t$physMatType\n";	
-        }      
+  
       }  
       
     }
 }
 
 #close the input/output files
-close(INFILE);
+
 close(OUTFILE);
 close(OUTFILE2);
 if (-z $rejectFileName) {
