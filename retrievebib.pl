a61d4502dc84073fa1c9294ce50864a44cfd0815
Fred Reiss <freiss@ou.edu>
2017-07-07 10:46:09 -0500

    Update retrievebib.pl
    corrected file test

--- retrievebib.pl
+++ retrievebib.pl
@@ -37,34 +37,33 @@ while(<INFILE>) {
   ($barcode) = split /\t/;
   $xmlFileInput = "./itemXML/iteminfo_$barcode.xml";
   #now we have some variable fields populated, particularly the barcode.  Let's go get some other metadata by using an Alma API call.  There are more graceful ways to accomplish this, but I often end up running fetch/parse scripts multiple times to augment results, so I favor a system-level curl call that creates XML files on the local machine, and then comment out the API call in future script runs and just act on the data files it already created
-  if (-x $xmlFileInput) {
+  if (-s $xmlFileInput) {
     #do something
   } else {
     system("curl -L --request GET 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/items?item_barcode=$barcode&apikey=l7xxc9bd7984f951474a8974d6ed0ef3d712' > ./itemXML/iteminfo_$barcode.xml");
   }
   #now we have an XML file ready for parsing; we'll specify that filename in a variable and then start up the XML parser
   if(-s $xmlFileInput) {
-      my $parser = XML::LibXML->new();
-      $copyID = "";
-      $holdingID = "";
-      $callnum ="";
-      $pid = "";
-      $description = "";
-      $physMatType ="";
-      $library = "";
-      $location = "";
-      $title = "";
-      $author = "";
-      $mmsID = "";
+    my $parser = XML::LibXML->new();
+    $copyID = "";
+    $holdingID = "";
+    $callnum ="";
+    $pid = "";
+    $description = "";
+    $physMatType ="";
+    $library = "";
+    $location = "";
+    $title = "";
+    $author = "";
+    $mmsID = "";
 
-      #assign the incoming file to a parser-related variable
-      my $itemdoc = $parser->parse_file($xmlFileInput);
-      #probably important to note at this point you need to know the general XML structure so you know what/where to get desired data in the next few commands
-      #let us create a for/each loop for item sections in the XML, and then a sub loop for each interesting section
-      #in the sub-loops, we'll fetch values in which we are interested
-      #should only be one of each item, holdingdata, and itemdata section, but this is useful logic when you have a doc with many items and repeating subsections
-      foreach my $itemrecord ($itemdoc->findnodes('/item')) {
-
+    #assign the incoming file to a parser-related variable
+    my $itemdoc = $parser->parse_file($xmlFileInput);
+    #probably important to note at this point you need to know the general XML structure so you know what/where to get desired data in the next few commands
+    #let us create a for/each loop for item sections in the XML, and then a sub loop for each interesting section
+    #in the sub-loops, we'll fetch values in which we are interested
+    #should only be one of each item, holdingdata, and itemdata section, but this is useful logic when you have a doc with many items and repeating subsections
+    foreach my $itemrecord ($itemdoc->findnodes('/item')) {
         #we need copy_ID, holding_ID from holding_data section
         foreach my $holdingDataSec ($itemrecord->findnodes('./holding_data')) {
           $callnum = $holdingDataSec->findnodes('./call_number')->to_literal(); 
@@ -93,7 +92,7 @@ while(<INFILE>) {
 
     #we need to pull the publication date by doing an additional API call for 
     $bibFile = "./itemXML/bib" . $mmsID . ".xml";
-    if (-x $bibFile) {
+    if (-s $bibFile) {
       #do something
     } else {
       system("curl -L --request GET 'https://api-eu.hosted.exlibrisgroup.com/almaws/v1/bibs/$mmsID?view=full&expand=None&apikey=l7xxa730c5d8d8844676ab5e1607d1161322' > $bibFile");  
